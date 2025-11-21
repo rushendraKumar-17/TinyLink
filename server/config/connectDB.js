689981db304cdc5cfbbff3f876da38399198333b
rushendra-scaleorange <rushendra@scaleorange.com>
2025-11-21 17:20:13 +0530

    db url changed

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async() => {
     try {
-        const response = await mongoose.connect("mongodb://pvkreddy2003:pvkr2003@ac-uatvnzj-shard-00-00.oo6lmbz.mongodb.net:27017,ac-uatvnzj-shard-00-01.oo6lmbz.mongodb.net:27017,ac-uatvnzj-shard-00-02.oo6lmbz.mongodb.net:27017/job-portal?ssl=true&replicaSet=atlas-k94kz4-shard-0&authSource=admin&retryWrites=true&w=majority/onlineCompiler");
+        const response = await mongoose.connect("mongodb://pvkreddy2003:pvkr2003@ac-uatvnzj-shard-00-00.oo6lmbz.mongodb.net:27017,ac-uatvnzj-shard-00-01.oo6lmbz.mongodb.net:27017,ac-uatvnzj-shard-00-02.oo6lmbz.mongodb.net:27017/job-portal?ssl=true&replicaSet=atlas-k94kz4-shard-0&authSource=admin&retryWrites=true&w=majority/TinyLink");
 
         console.log("Connected to database");
         
